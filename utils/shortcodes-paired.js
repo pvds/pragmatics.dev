bc809c027db3a768c05f323756badee94a88bdf0
Peter van der Steen <peter.van.der.steen@priva.nl>
2020-10-03 13:26:29 +0300

    feat: update styling & implement contentSection shortcode

--- utils/shortcodes-paired.js
+++ utils/shortcodes-paired.js
@@ -7,11 +7,23 @@ module.exports = {
    */
   contentBlock: (content, options = {}) => {
     const { fields = {}, classes = '' } = options;
-    const titleHTML = !fields.title ? '' : `<h2 class="content-block__title">${fields.title}</h2>`;
+    const titleHTML =
+      fields.title && fields.showTitle
+        ? `<h2 class="content-block__title">${fields.title}</h2>`
+        : '';
     return `
-      <section class="content-block ${classes}">
+      <div class="content-block ${classes}">
         ${titleHTML}
         <div class="content-block__main plain-text">${content}</div>
+      </div>`;
+  },
+  contentSection: (content, options = {}) => {
+    const { classes = '' } = options;
+    return `
+      <section class="content-section ${classes}">
+        <div class="content-section__inner">
+            ${content}
+        </div>
       </section>`;
   },
 };
