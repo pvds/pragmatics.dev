87011d2b605033a2e087de9b31ba201840c0f366
Peter van der Steen <peter.van.der.steen@priva.nl>
2020-10-04 17:51:53 +0300

    feat: filter page components by type

--- utils/filters.js
+++ utils/filters.js
@@ -3,36 +3,51 @@ const { DateTime } = require('luxon');
 module.exports = {
   /**
    * Format date to given format
-   * @param {string} date
+   * @param {Date} date
    * @param {string} format
    * @return {string}
    */
-  dateToFormat: function (date, format) {
-    return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(String(format));
-  },
+  dateToFormat: (date, format) =>
+    DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(String(format)),
 
   /**
    * Format date to ISO format
-   * @param {string} date
+   * @param {Date} date
    * @return {string}
    */
-  dateToISO: function (date) {
-    return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
+  dateToISO: (date) =>
+    DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
       includeOffset: false,
       suppressMilliseconds: true,
-    });
-  },
+    }),
 
   /**
    * Obfuscate string
    * @param {string} str
    * @return {string}
    */
-  obfuscate: function (str) {
+  obfuscate: (str) => {
     const chars = [];
     for (let i = str.length - 1; i >= 0; i--) {
       chars.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
     }
     return chars.join('');
+  },
+
+  /**
+   * Check whether entry has components of a specific type
+   * @param {Array} components
+   * @param {string} type
+   * @return {[]}
+   */
+  componentsOfType: (components, type) => {
+    let filtered = [];
+    filtered = components?.filter(function (component) {
+      if (component.sys.contentType.sys.id === type) {
+        return component;
+      }
+    });
+
+    return filtered;
   },
 };
